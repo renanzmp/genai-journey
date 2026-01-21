3bc5b2ca5814c210db417602c135cb2f83e5ce20
Renan Pereira <renanpereira2005@gmail.com>
2026-01-21 17:18:26 -0300

    Dia 3 de novo fazendo list comprehension

--- 01-python-review/day3_list_comprehension.py
+++ 01-python-review/day3_list_comprehension.py
@@ -1,15 +1,16 @@
 '''
 with open('01-python-review/data/pessoas.txt', 'r') as pessoas:
     lista_pessoas = []
-    lista_atualizada = []
     for pessoa in pessoas:
         lista_pessoas.append(pessoa.strip().split(','))
 
     for pessoa in lista_pessoas:
         pessoa[1] = int(pessoa[1])
 
+    lista_atualizada = []
+
     for pessoa in lista_pessoas:
-        if pessoa[1] >= 18:
+        if pessoa[1] > 18:
             dicionario_pessoas = dict({'nome': pessoa[0], 'idade': pessoa[1]})
             lista_atualizada.append(dicionario_pessoas)
 
@@ -18,18 +19,16 @@ with open('01-python-review/data/pessoas.txt', 'r') as pessoas:
 
 with open('01-python-review/data/pessoas.txt', 'r') as pessoas:
     lista_pessoas = []
-    lista_atualizada = []
     for pessoa in pessoas:
         lista_pessoas.append(pessoa.strip().split(','))
 
     for pessoa in lista_pessoas:
         pessoa[1] = int(pessoa[1])
 
-    lista_atualizada = []
-
-    for pessoa in lista_pessoas:
-        if pessoa[1] >= 18:
-            dicionario_pessoas = dict({'nome': pessoa[0], 'idade': pessoa[1]})
-            lista_atualizada.append(dicionario_pessoas)
+    lista_atualizada = [
+        {'nome': pessoa[0], 'idade': pessoa[1]}
+        for pessoa in lista_pessoas
+        if pessoa[1] > 18
+    ]
 
     print(lista_atualizada)
